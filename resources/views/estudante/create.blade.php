6570406ac1258aa2cc352e6cf2d0be944d958728
Adilson19 <adilsonsousaas82@gmail.com>
2025-03-07 16:50:25 +0100

    fix: repararos codigos das views

---
+++ resources/views/estudante/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <a href="/estudantes">Lista Estudantes</a>
+    @if (session('success'))
+        <p>{{ session('success') }}</p>
+    @endif
+
+    @if (session('error'))
+        <p>{{ session('error') }}</p>
+    @endif
+
+    <form action="/estudantes" method="post">
+        @method('POST')
+        @csrf
+
+        <input type="text" name='nome' placeholder="Nome" />
+        @if($errors->has('nome'))
+            <p>{{ $errors->first('nome') }}</p>
+        @endif
+        <br/>
+        <input type="date" name="data_de_nascimento" placeholder="Data" />
+        @if($errors->has('data_de_nascimento'))
+            <p>{{ $errors->first('turma') }}</p>
+        @endif
+        </br>
+
+        <button type="submit">Salvar</button>
+
+    </form>
---
+++ resources/views/estudante/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.app')
+@section('content')
+<a href="/estudantes/create">Novo Estudante</a>
+    <table border="1" width="50%"
+        style="
+            color:red;
+            background-color: yellow;
+            text-align: center;
+        ">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Data de Nascimento</th>
+                <th>Turma</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($estudantes as $estudante)
+                <tr>
+                    <td>{{ $estudante->pessoa->nome }}</td>
+                    <td>{{ $estudante->pessoa->data_de_nascimento }}<td>
+                    <td>{{ $estudante->turma }}</td>
+                    <td>
+                        <a href="/estudantes/{{ $estudante->id }}/edit">Editar</a>
+                        {{-- Criando o botao de delete --}}
+                        <form action="/estudantes/{{ $estudante->id }}" method="post">
+                            @csrf {{-- Gerando os tookens --}}
+                            @method('DELETE'){{-- O metodo delete que vai ser utilizado --}}
+                            <button type="submit">eliminar</button>
+                        </form>
+                        {{-- Fim da Criacao do botao de delete --}}
+                        
+                        {{-- SHOW --}}
+                        <a href="/estudantes/{{ $estudante->id }}">Detalhes</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
