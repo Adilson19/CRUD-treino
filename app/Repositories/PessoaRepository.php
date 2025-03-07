787a6a2654da25a07c2194778d7ca67a25eaba94
Adilson19 <adilsonsousaas82@gmail.com>
2025-03-07 10:05:14 +0100

    feat: criar os repositories

---
+++ app/Repositories/PessoaRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CRUDInterface;
+use App\Models\Estudante;
+
+class EstudanteRepository implements CRUDInterface
+{
+    protected $model;
+    //  Implementando as interfaces em models que faz a conexao direta com o banco de dados
+    public function __construct(Estudante $estudante)
+    {
+        $this->model = $estudante;
+    }
+
+    public function findAll()
+    {
+        return $this->model->all();
+    }
+
+    public function findOrFail(string $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, string $id)
+    {
+        $this->model->findOrFail($id)->update($data);
+    }
+
+    public function delete(string $id)
+    {
+        $this->model->findOrFail($id)->delete();
+    }
+}
