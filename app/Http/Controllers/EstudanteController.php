881b03e736bc407deffaa6dd0ac47fe218cd1d29
Adilson19 <adilsonsousaas82@gmail.com>
2025-03-07 12:22:51 +0100

    feat: criar um controller para o estudante

---
+++ app/Http/Controllers/EstudanteController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\EstudanteService;
+use App\Http\Requests\EstudanteStoreRequest;
+use Illuminate\Http\Request;
+
+class EstudanteController extends Controller
+{
+    //
+    private $estudanteService;
+
+    public function __construct(EstudanteService $estudanteService)
+    {
+        $this -> estudanteService = $estudanteService;
+    }
+
+    public function index()
+    {
+        $infoForView = $this -> estudanteService -> getInfoForIndexView();
+
+        return view('estudante.index', $infoForView);
+    }
+
+    public function create()
+    {
+        $infoForView = $this -> estudanteService -> getInfoForCreateView();
+
+        return view('estudante.create', $infoForView);
+    }
+
+    public function store(EstudanteStoreRequest $estudanteStoreRequest)
+    {
+        $response = $this -> estudanteService -> store($estudanteStoreRequest -> all());
+        if($response){
+            return back() -> with('success', 'feito com sucesso');
+        }
+        return back()->with('error', 'falha ao salvar');
+    }
+
+    public function show($id)
+    {
+        $infoForView = $this -> estudanteService -> getInfoForShowView($id);
+
+        return view('estudante.show', $infoForView);
+    }
+
+    public function edit(string $id)
+    {
+        $infoForView = $this -> estudanteService -> getInfoForEditView($id);
+
+        return view('estudante.edit', $infoForView);
+    }
+
+    public function update(EstudanteUpdateRequest $estudanteUpdateRequest, string $id)
+    {
+        $response = $this -> estudanteService -> update($estudanteUpdateRequest -> all(), $id);
+        if($response){
+            return back()->with('success', 'feito com sucesso');
+        }
+        return back()->with('error', 'nao foi possivel');
+    }
+
+    public function destroy(string $id)
+    {
+        $reponse = $this -> estudanteService -> destroy($id);
+        if($reponse){
+            return back() -> with('success', 'feito com sucesso');
+        }
+        return back()->with('error', 'nao foi possivel');
+    }
+}
